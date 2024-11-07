e241f72ad95915d2fecaf7b07f6b95482f150757
유열 <vflag21c@nate.com>
2024-11-07 21:36:39 +0900

    JAVA - 네트워크 & 종료

---
+++ src/network/exception/connect/SoTimeoutClient.java
@@ -0,0 +1,22 @@
+package network.exception.connect;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class SoTimeoutClient {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 12345);
+        InputStream input = socket.getInputStream();
+
+        try {
+            socket.setSoTimeout(3000);
+            input.read();
+            System.out.println("read = " + input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        socket.close();
+    }
+}
