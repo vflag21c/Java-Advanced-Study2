4609da5a50a2d0f07f37a41ae133f0ad908283b2
유열 <vflag21c@nate.com>
2024-11-14 23:51:26 +0900

    JAVA - 리플렉션 서블릿

---
+++ src/reflection/ConstructV1.java
@@ -0,0 +1,24 @@
+package reflection;
+
+import java.lang.reflect.Constructor;
+
+public class ConstructV1 {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> aClass = Class.forName("reflection.data.BasicData");
+
+        // public method
+        System.out.println("== constructors() ==");
+        Constructor<?>[] constructors = aClass.getConstructors();
+        for (Constructor<?> constructor : constructors) {
+            System.out.println(constructor);
+        }
+
+        // all
+        System.out.println("== declaredConstructors() ==");
+        Constructor<?>[] declaredConstructors = aClass.getDeclaredConstructors();
+        for (Constructor<?> declaredConstructor : declaredConstructors) {
+            System.out.println(declaredConstructor);
+        }
+    }
+}
